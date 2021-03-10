35e3e2adefe984a4d3fa189755c1924244803e91
john4ster <johnforsterbusiness@gmail.com>
2021-03-10 13:46:42 -0500

    fix minor bugs

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # My Top Spotify Info
 Web app that allows the user to login with their Spotify account and see their top 20 tracks and top 20 artists using the Spotify API.
 
+# How to run
+* Must have a clientID and clientSecret from Spotify for Developers
+* In server/authorization_code/app.js, replace the client_id and client_secret variables with your own
+* Run "nodemon app.js" from the authorization_code directory
+* Go to localhost:8888 and login
+
 # What I Learned
 * How an API works
 * How to get data from an API and display it to the user
--- server/authorization_code/app.js
+++ server/authorization_code/app.js
@@ -1,18 +1,11 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var env = require('dotenv').config();
 
 var client_id = process.env.clientID; // Your client id
 var client_secret = process.env.clientSecret; // Your secret
